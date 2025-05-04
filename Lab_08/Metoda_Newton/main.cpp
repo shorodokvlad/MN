81044448cc6e50678a549bd566782766731f85f7
Vladislav Shorodok <vlad.shorodoc@gmail.com>
2025-05-04 16:29:51 +0300

    Update Lab_08

--- Lab_08/Metoda_Newton/main.cpp
+++ Lab_08/Metoda_Newton/main.cpp
@@ -1,35 +1,46 @@
 /* Metoda Newton */
-#include<conio.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <iostream>
 #include<math.h>
-float f(float);
-float a,b,x[1000],y[1000],z[1000], p, h, s;
-int i,n;
-int main ()
-{
-    printf("introduceti limita din stanga a:");
-    scanf("%f",&a);
-    printf("\nintroduceti limita din dreapta b:");
-    scanf("%f",&b);
-    printf("introduceti numarul de subintervale n:");
-    scanf("%d",&n);
-    h=(b-a)/n;
-    for(i=0; i<=n; i++)
-        x[i]=a+i*h;
-    s=f(a)+f(b);
-    for(i=1; i<=n-1; i++)
-        s=s+2*f(x[i]);
-    p=0;
-    for(i=1; i<=n; i++)
-        y[i]=x[i-1]+h/3;
-    for(i=1; i<=n; i++)
-        z[i]=x[i-1]+2*h/3;
-    for(i=1; i<=n; i++)
-        p=p+3*(f(y[i])+f(z[i]));
-    s=(s+p)*h/8;
-    printf(" valoarea aproximativa a integralei este I=%f",s);
-}
+using namespace std;
+
+float a, b, x[1000], y[1000], z[1000], p, h, s;
 float f(float x)
 {
-    return x*x+3*x;
+    return x * x + 3 * x;
+}
+int i, n;
+
+int main ()
+{
+    cout << "introduceti limita din stanga a: ";
+    cin >> a;
+
+    cout << "introduceti limita din dreapta a: ";
+    cin >> b;
+
+    cout << "introduceti numarul de subintervale: ";
+    cin >> n;
+
+    h = (b - a) / n;
+    for (i = 1; i <= n; i++)
+        x[i] = a + i * h;
+    s = f(a) + f(b);
+
+    for (i = 1; i <= n - 1; i++)
+        s = s + 2 * f(x[i]);
+
+    for (i = 1; i <= n; i++)
+        y[i] = x[i - 1] + h / 3;
+
+    for (i = 1; i <= n; i++)
+        z[i] = x[i - 1] + 2 * h / 3;
+
+    for (i = 1; i <= n; i++)
+        p = p + 3 * (f(y[i]) + f(z[i]));
+
+    s = (s + p) * h / 8;
+
+
+    printf(" valoarea aproximativa a integralei este I=%f",s);
 }
--- Lab_08/Metoda_Simpson/main.cpp
+++ Lab_08/Metoda_Simpson/main.cpp
@@ -1,30 +1,45 @@
 /* Metoda Simpson */
-#include<conio.h>
-#include<stdio.h>
-float f(float);
-float a,b,x[1000],y[1000], p, h, s;
-int i,n;
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+float a, b, x[1000], y[1000], p, h, s;
+float f(float x)
+{
+    return x * x + 3 * x;
+}
+int i, n;
+
+
 int main ()
 {
-printf("introduceti limita din stanga a:");
-scanf("%f",&a);
-printf("\nintroduceti limita din dreapta b:");
-scanf("%f",&b);
-printf("introduceti numarul de subintervale n:");
-scanf("%d",&n);
-h=(b-a)/n;
-for(i=0;i<=n;i++)
-x[i]=a+i*h;
-s=f(a)+f(b);
-for(i=1;i<=n-1;i++)
-s=s+2*f(x[i]);
-p=0;
-for(i=1;i<=n;i++)
-y[i]=(x[i-1]+x[i])/2;
-for(i=1;i<=n;i++)
-p=p+4*f(y[i]);
-s=(s+p)*h/6;
-printf(" valoarea aproximativa a integralei este I=%f",s);
+    cout << "introduceti limita din stanga a: ";
+    cin >> a;
+
+    cout << "introduceti limita din dreapta a: ";
+    cin >> b;
+
+    cout << "introduceti numarul de subintervale: ";
+    cin >> n;
+
+    h = (b - a) / n;
+
+    for (i = 1; i <= n; i++)
+        x[i] = a + i * h;
+    s = f(a) + f(b);
+
+    for (i = 1; i <= n - 1; i++)
+        s = s + 2 * f(x[i]);
+
+    for (i = 1; i <= n; i++)
+        y[i] = (x[i] - 1 + x[i]) / 2;
+
+    for (i = 1; i <= n; i++)
+        p = p + 4 * f(y[i]);
+
+    s = (s + p) * h / 6;
+
+    printf(" valoarea aproximativa a integralei este I=%f",s);
 }
-float f(float x)
-{return x*x+3*x;}
